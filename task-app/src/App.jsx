20cd29431fb7293781c027e38085f182cf551607
Edward Santos <99231129+rhidicus02@users.noreply.github.com>
2024-03-06 13:01:03 +0800

    Started

---
+++ task-app/src/App.jsx
@@ -0,0 +1,11 @@
+import TaskApp from "./page/task-app/task-main";
+
+function App() {
+  return (
+    <>
+      <TaskApp />
+    </>
+  );
+}
+
+export default App;
---
+++ task-app/src/page/task-app/task-main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./task-main.scss";
+
+const taskMain = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="red text-center">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default taskMain;
